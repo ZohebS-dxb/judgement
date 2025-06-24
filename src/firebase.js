4e18e39a1177058444c2c9d5e6baf82af9784d0f
ZohebS-dxb <zubisid@gmail.com>
2025-06-25 03:57:47 +0400

    Initial commit for Judgement Score App

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, get, set, update } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  databaseURL: 'https://YOUR_PROJECT_ID.firebaseio.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+ apiKey: "AIzaSyClffvlLY66cKD6ex6eOygg8uS9rbKvCns",
+  authDomain: "liar-1-ebc12.firebaseapp.com",
+  databaseURL: "https://liar-1-ebc12-default-rtdb.europe-west1.firebasedatabase.app",  // ✅ Add this
+  projectId: "liar-1-ebc12",
+  storageBucket: "liar-1-ebc12.firebasestorage.app",
+  messagingSenderId: "853929867266",
+  appId: "1:853929867266:web:12fd05aa5cfdc0746d0f1b"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
